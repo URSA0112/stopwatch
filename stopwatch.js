56f56c16fcc90ebea735b0ba2eeb3e8498532f8d
URSA0112 <Nimone99@gmail.com>
2025-01-26 20:41:26 +0800

    js stopwatch backbone added

--- stopwatch.js
+++ stopwatch.js
@@ -3,12 +3,15 @@ const startButtonElRef = document.getElementById('startbutton')
 const stopButtonElRef = document.getElementById('stopbutton')
 const resetButtonElRef = document.getElementById('resetbutton')
 
-startButtonElRef.addEventListener('click',(startTimer) )
-stopButtonElRef.addEventListener('click',(stopTimer) )
-resetButtonElRef.addEventListener('click', (resetTimer) )
-
+let hour  ;
+let min ; 
+let second; 
 const startTimer = ()=>{};
 const stopTimer = ()=>{};
 const resetTimer = ()=>{};
 
 
+
+startButtonElRef.addEventListener('click',startTimer )
+stopButtonElRef.addEventListener('click',stopTimer )
+resetButtonElRef.addEventListener('click', resetTimer )
