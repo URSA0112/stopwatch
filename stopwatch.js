e9bea5b369d5b4abc64336ff44e1141cf4b175c8
URSA0112 <Nimone99@gmail.com>
2025-01-26 19:57:14 +0800

    started html css done back bone done

---
+++ stopwatch.js
@@ -0,0 +1,14 @@
+const TimerElRef = document.getElementById('timer')
+const startButtonElRef = document.getElementById('startbutton')
+const stopButtonElRef = document.getElementById('stopbutton')
+const resetButtonElRef = document.getElementById('resetbutton')
+
+startButtonElRef.addEventListener('click',(startTimer) )
+stopButtonElRef.addEventListener('click',(stopTimer) )
+resetButtonElRef.addEventListener('click', (resetTimer) )
+
+const startTimer = ()=>{};
+const stopTimer = ()=>{};
+const resetTimer = ()=>{};
+
+
